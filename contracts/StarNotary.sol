fe3c9a8827d49ebe1b99938f9d39c814c02e6265
Philippe Timothe <PhilippeTimothe@gmail.com>
2022-04-01 11:09:02 -0400

    implement symbol,token name, exhcange and transfer functions

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -12,6 +12,9 @@ contract StarNotary is ERC721 {
         string name;
     }
 
+    string public tokenName;
+    string public tokenSymbol;
+
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
@@ -58,20 +61,29 @@ contract StarNotary is ERC721 {
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
+        return tokenIdToStarInfo[_tokenId];
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        require(msg.sender == ownerOf(_tokenId1) || msg.sender == ownerOf(_tokenId2), "You can't exchange a star you don't own");
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId2)
+        address ownerAddress1 = ownerOf(_tokenId1);
+        address ownerAddress2 = ownerOf(_tokenId2);
         //4. Use _transferFrom function to exchange the tokens.
+        _transferFrom(ownerAddress1, ownerAddress2, _tokenId1);
+        _transferFrom(ownerAddress2, ownerAddress1, _tokenId2);
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
+        require(msg.sender == ownerOf(_tokenId), "You can't transfer a star you don't own");
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        address ownerAddress = ownerOf(_tokenId);
+        _transferFrom(ownerAddress, _to1, _tokenId);
     }
 
 }
